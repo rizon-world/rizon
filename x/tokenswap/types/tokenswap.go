a0712e6309f961c74ea399e2da25889f2f95ccf7
yoosah <yoosah@hdac.io>
2021-06-29 17:26:33 +0900

    feat: add swap limitation parameter

--- x/tokenswap/types/tokenswap.go
+++ x/tokenswap/types/tokenswap.go
@@ -4,6 +4,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+const (
+	SwapLimitation = 2_800_000_000
+)
+
 // Newtokenswap creates a new swap state object
 func NewTokenswap(txHash string, receiver string, signer string, amount sdk.Coins) Tokenswap {
 	return Tokenswap{
