303e4b88858fd38808e6e3870290f11ccb34c23b
yoosah <yoosah@hdac.io>
2021-08-26 13:43:16 +0900

    feat: introduce rizon max supply limitation
    This amount comes from legacy HDAC blockchain parameter.

--- x/tokenswap/types/tokenswap.go
+++ x/tokenswap/types/tokenswap.go
@@ -4,11 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-const (
-	MaxSupply      = 2_851_982_500_000_000
-	SwapLimitation = 1_500_000_000_000_000 // estimation of maximum swappable amount
-)
-
 // NewTokenswap creates a new swap state object
 func NewTokenswap(txHash string, receiver string, signer string, amount sdk.Coins) Tokenswap {
 	return Tokenswap{
