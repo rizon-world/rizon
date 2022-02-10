b90d0a8fff1853eb065d11bef7bab612392840b5
Galadrin <dapie@cros-nest.com>
2022-02-10 13:03:27 +0100

    fix: add missing capability beginBlocker
    This patch fix the IBC channel creation issue.
    
    Signed-off-by: Galadrin <dapie@cros-nest.com>

--- app/app.go
+++ app/app.go
@@ -368,7 +368,7 @@ func NewRizonApp(
 	// CanWithdrawInvariant invariant.
 	// NOTE: staking module is required if HistoricalEntries param > 0
 	app.mm.SetOrderBeginBlockers(
-		upgradetypes.ModuleName, minttypes.ModuleName, distrtypes.ModuleName, slashingtypes.ModuleName,
+		upgradetypes.ModuleName, capabilitytypes.ModuleName, minttypes.ModuleName, distrtypes.ModuleName, slashingtypes.ModuleName,
 		evidencetypes.ModuleName, stakingtypes.ModuleName, ibchost.ModuleName,
 	)
 	app.mm.SetOrderEndBlockers(crisistypes.ModuleName, govtypes.ModuleName, stakingtypes.ModuleName, treasurytypes.ModuleName)
